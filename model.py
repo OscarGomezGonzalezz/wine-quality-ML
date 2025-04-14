aec2fafa599dc9fcc2c631963a725f04e64b6d44
Julio Galán <juliogalansoler@gmail.com>
2025-04-14 12:10:25 +0200

    import changes

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
 
 # Leer el CSV
 data = pd.read_csv('./data/winequality-red.csv', sep=';')
@@ -32,8 +35,7 @@ X_scaled = scaler.fit_transform(X)
 # Separar en train y test
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+
 
 model = RandomForestClassifier(n_estimators=200, random_state=42)
 model.fit(X_train, y_train)
@@ -45,7 +47,7 @@ y_pred = model.predict(X_test)
 print(classification_report(y_test, y_pred))
 print(confusion_matrix(y_test, y_pred))
 
-import numpy as np
+
 
 cm = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(8,6))
